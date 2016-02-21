6fb2fb38bed4ee988e9f42b162b571799a3100c6
Emanuel Borsboom <emanuel.borsboom@gmail.com>
2016-02-21 10:34:54 -0800

    Update with_kubeconfigs.sh

--- kubernetes/with_kubeconfigs.sh
+++ kubernetes/with_kubeconfigs.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env bash
-parallel echo KUBECONFIG={}\; export KUBECONFIG={}\; $@ ::: $KUBECONFIGS
+# Run a command in parallel for multiple Kubernetes clusters.
+# Set the KUBECONFIG environment variable to a space-separated list
+# of paths to `kubeconfig` files.
+exec parallel echo KUBECONFIG={}\; export KUBECONFIG={}\; $@ ::: $KUBECONFIGS
