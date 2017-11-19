c0012af9b3d4786f6beb6fc0fdfa4830b3727e48
Emanuel Borsboom <manny@fpcomplete.com>
2017-11-19 05:34:55 -0800

    kube-ns: add --reset argument

--- kubernetes/kube-ns.sh
+++ kubernetes/kube-ns.sh
@@ -16,31 +16,50 @@
 # Another way to achieve this sort of result might be to make a `kubectl`
 # wrapper script and make sure it's on the PATH instead of the `kubectl` binary.
 #
+# The optional `--reset` argument will reset the environment to what it was
+# beore kube-ns was used.  It can be used with eval or as a wrapper.
+#
 
 set -eu
 
-if [[ -s "${KUBE_NS_ORIG_KUBECONFIG:-}" ]]; then
+if [[ -s "${KUBE_NS_ORIG_KUBECONFIG:-}" && "${KUBECONFIG:-}" == "${KUBE_NS_EXPECT_KUBECONFIG:-}" ]]; then
     export KUBECONFIG="$KUBE_NS_ORIG_KUBECONFIG"
 else
     export KUBECONFIG="${KUBECONFIG:-$HOME/.kube/config}"
 fi
-export KUBE_NS_ORIG_KUBECONFIG="$KUBECONFIG"
+unset KUBE_NS_ORIG_KUBECONFIG
+unset KUBE_NS_EXPECT_KUBECONFIG
 
 if [[ $# -eq 0 ]]; then
     echo "$0: requires at least one argument" >&2
     exit 1
 fi
-NAMESPACE="$1"
-shift
-
-export KUBECONFIG="${KUBECONFIG}_ns_${NAMESPACE}"
-cp "$KUBE_NS_ORIG_KUBECONFIG" "$KUBECONFIG"
-CONTEXT="$(kubectl config current-context)"
-kubectl config set-context "$CONTEXT" --namespace="$NAMESPACE" >/dev/null
-
-if [[ $# -gt 0 ]]; then
-    exec "$@"
+if [[ "$1" == "--reset" ]]; then
+    shift
+    if [[ $# -gt 0 ]]; then
+        exec "$@"
+    else
+        printf "export KUBECONFIG=%q;\n" "$KUBECONFIG"
+        printf "unset KUBE_NS_ORIG_KUBECONFIG\n"
+        printf "unset KUBE_NS_EXPECT_KUBECONFIG\n"
+    fi
 else
-    printf "export KUBECONFIG=%q;\n" "$KUBECONFIG"
-    printf "export KUBE_NS_ORIG_KUBECONFIG=%q;\n" "$KUBE_NS_ORIG_KUBECONFIG"
+    NAMESPACE="$1"
+
+    shift
+
+    export KUBE_NS_ORIG_KUBECONFIG="$KUBECONFIG"
+    export KUBECONFIG="${KUBECONFIG}_ns_${NAMESPACE}"
+    export KUBE_NS_EXPECT_KUBECONFIG="${KUBECONFIG}"
+    cat "$KUBE_NS_ORIG_KUBECONFIG" >"$KUBECONFIG"
+    CONTEXT="$(kubectl config current-context)"
+    kubectl config set-context "$CONTEXT" --namespace="$NAMESPACE" >/dev/null
+
+    if [[ $# -gt 0 ]]; then
+        exec "$@"
+    else
+        printf "export KUBECONFIG=%q;\n" "$KUBECONFIG"
+        printf "export KUBE_NS_ORIG_KUBECONFIG=%q;\n" "$KUBE_NS_ORIG_KUBECONFIG"
+        printf "export KUBE_NS_EXPECT_KUBECONFIG=%q;\n" "$KUBE_NS_EXPECT_KUBECONFIG"
+    fi
 fi
